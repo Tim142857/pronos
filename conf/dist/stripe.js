5bd3e3fc6cfa4c82b054297c15fb37a28b9278ac
Tim <timothyleroch@gmail.com>
2019-06-20 14:47:04 +0200

    conf db

---
+++ conf/dist/stripe.js
@@ -0,0 +1,4 @@
+module.exports = {
+  KEY_TEST: 'pk_test_zopRw2rbITNrU0FAIQqX499M004cqaj1gO',
+  ID_PRODUCT: 'prod_FExD39vYIhPQEi'
+};
--- conf/local/mysql.js
+++ conf/local/mysql.js
@@ -1,9 +1,20 @@
+// module.exports = {
+//   host: 'localhost',
+//   port: 3306,
+//   db: 'pronos',
+//   user: 'root',
+//   password: 'user',
+//   dialect: 'mysql', // mariadb, mysql
+//   // DEV - display MySQL logs in stdout when true
+//   inspect: false
+// }
+
 module.exports = {
-  host: 'localhost',
+  host: 'eu-cdbr-west-02.cleardb.ne',
   port: 3306,
-  db: 'pronos',
-  user: 'root',
-  password: 'user',
+  db: 'heroku_35a3f75ec8405f1',
+  user: 'b8c6e02fe419cb',
+  password: '14eff41a',
   dialect: 'mysql', // mariadb, mysql
   // DEV - display MySQL logs in stdout when true
   inspect: false
