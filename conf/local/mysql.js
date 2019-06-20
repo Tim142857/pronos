75d96885fcbec636290d907ee522bb29d7302db6
Tim <timothyleroch@gmail.com>
2019-06-20 15:03:37 +0200

    patch db config

--- conf/local/mysql.js
+++ conf/local/mysql.js
@@ -8,7 +8,6 @@
 //   // DEV - display MySQL logs in stdout when true
 //   inspect: false
 // }
-mysql://b8c6e02fe419cb:14eff41a@eu-cdbr-west-02.cleardb.net/heroku_35a3f75ec8405f1?reconnect=true
 module.exports = {
   host: 'eu-cdbr-west-02.cleardb.net',
   // port: 3306,
