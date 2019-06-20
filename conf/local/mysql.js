bb5a8f62ed39e4a9caf4eeb7e16e7d58f5bfefaa
Tim <timothyleroch@gmail.com>
2019-06-20 14:52:04 +0200

    patch db config

--- conf/local/mysql.js
+++ conf/local/mysql.js
@@ -8,10 +8,10 @@
 //   // DEV - display MySQL logs in stdout when true
 //   inspect: false
 // }
-
+mysql://b8c6e02fe419cb:14eff41a@eu-cdbr-west-02.cleardb.net/heroku_35a3f75ec8405f1?reconnect=true
 module.exports = {
-  host: 'eu-cdbr-west-02.cleardb.ne',
-  port: 3306,
+  host: 'eu-cdbr-west-02.cleardb.net',
+  // port: 3306,
   db: 'heroku_35a3f75ec8405f1',
   user: 'b8c6e02fe419cb',
   password: '14eff41a',
